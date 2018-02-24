5bb0d0045d0daa7da39c44526a48f2c03ce53549
marek <kursygitarowekopernik@gmail.com>
2018-02-24 23:50:08 +0100

    dodana autoryzacja google

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    'GOOGLE_CLIENT_ID': '387896936917-1t5eg5rh2a34d4onfa0c7eb443rq4ul3.apps.googleusercontent.com',
+    'GOOGLE_CLIENT_SECRET': 'cvCXNtUu7OgZo60NPMnv1q7I',
+    'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
+}
